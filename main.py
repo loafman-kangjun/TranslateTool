27180a436901800794dd692bac19cb574dcc25fc
LoafMan <kangjun1120@hotmail.com>
2023-08-02 20:41:04 +0800

    init

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 import ui
+import util
 
 ui_main = ui.main.MainWindow
 
 if __name__ == "__main__":
+    util.file.setting('./setting.json')
     root = tk.Tk()
     app = ui_main(root)
     root.mainloop()
--- util/file.py
+++ util/file.py
@@ -1,10 +1,18 @@
 import json
-from tkinter import filedialog
+import os
+# from tkinter import filedialog
 
 
 # filedialog.askopenfilename(title="Select file", filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
 
 def setting(path):
-    with open(path, 'r') as file:
-        data = json.load(file)
+    if not os.path.exists(path):
+        data = {}
+        with open(path, 'w') as file:
+            json.dump(data, file)
+    else:
+        with open(path, 'r') as file:
+            data = json.load(file)
     return data
+
+# setting('./setting.json')
