22f67cf43efaf314144ed1914957394a88f34986
LoafMan <kangjun1120@hotmail.com>
2023-08-03 11:08:04 +0800

    init

--- main.py
+++ main.py
@@ -8,17 +8,16 @@ class MainWindow:
         self.root.geometry("400x600")
         self.root.resizable(0, 0)
 
-        # self.label_input = tk.Label(root, text="请输入要生草的内容：", font=("Microsoft YaHei", 11))
-        # self.label_input.grid(row=0, column=0, padx=5, pady=5)
-        self.root.button_setting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.onModal)
+        self.root.button_setting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.OpenModal)
         self.root.button_setting.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
 
         # self.root.mainloop()
-    def onModal(self):
-        moddal = Modal(root)
+    @staticmethod
+    def OpenModal():
+        moddal = SettingWindow(root)
 
 
-class Modal(Toplevel):
+class SettingWindow(Toplevel):
 
     def __init__(self, parent):
         Toplevel.__init__(self, parent)
@@ -30,6 +29,5 @@ class Modal(Toplevel):
 
 if __name__ == '__main__':
     root = Tk()
-    # app = Main()
     MainWindow(root)
     root.mainloop()
