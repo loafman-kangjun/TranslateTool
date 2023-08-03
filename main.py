750dd7777e7df9223e4e6cfd081e63dcd9f3654e
LoafMan <kangjun1120@hotmail.com>
2023-08-03 11:12:35 +0800

    init

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ class MainWindow:
         self.root.geometry("400x600")
         self.root.resizable(0, 0)
 
-        self.root.button_setting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.OpenModal)
+        self.root.button_setting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.OpenSetting)
         self.root.button_setting.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
 
-        # self.root.mainloop()
     @staticmethod
-    def OpenModal():
+    def OpenSetting():
         moddal = SettingWindow(root)
 
 
@@ -25,6 +24,12 @@ class SettingWindow(Toplevel):
         self.geometry("400x500")
         self.resizable(height=False, width=False)
         self.grab_set()
+
+        self.label = Label(self, text="这是设置窗口", font=("Microsoft YaHei", 11))
+        self.label.pack(pady=20)
+
+        self.button_ok = Button(self, text="确定", font=("Microsoft YaHei", 11))
+        self.button_ok.pack(pady=10)
 
 
 if __name__ == '__main__':
