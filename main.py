1a15e89f0e7ae84f4c60a1ae6aaffe556088f7bf
LoafMan <kangjun1120@hotmail.com>
2023-08-03 20:27:51 +0800

    init

--- main.py
+++ main.py
@@ -6,15 +6,29 @@ class MainWindow:
     def __init__(self, root):
         self.root = root
         self.root.title("生草机")
-        self.root.geometry("400x600")
+        self.root.geometry("600x400")
         self.root.resizable(0, 0)
 
-        self.button_setting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.OpenSetting)
-        self.button_setting.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
+        self.title_label = Label(root,text="输入原始内容：",anchor="center", font=("Microsoft YaHei", 11))
+        self.title_label.place(x=5, y=5, width=110, height=30)
+
+        self.original_text = Text(root)
+        self.original_text.place(x=5, y=40, width=575, height=140)
+        self.original_scroll = Scrollbar(root)
+        self.original_scroll.place(x=580, y=40, width=17, height=140)
+
+        # 两个控件关联
+        self.original_scroll.config(command=self.original_text.yview)
+        self.original_text.config(yscrollcommand=self.original_scroll.set)
+        # self.button_sestting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.OpenSetting)
+        # self.button_sestting.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
+        #
+        # self.button_setting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.OpenSetting)
+        # self.button_setting.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
 
     @staticmethod
     def OpenSetting():
-        moddal = SettingWindow(root)
+        _ = SettingWindow(root)
 
 
 class SettingWindow(Toplevel):
