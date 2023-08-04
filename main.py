8c2e1dfcee896ac541aeb63924009fa0ac40d9de
LoafMan <kangjun1120@hotmail.com>
2023-08-04 21:11:45 +0800

    init

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from tkinter import *
 import util
+import translates
+import random
 
 
 class MainWindow:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self, __root):
+        self.root = __root
         self.root.title("生草机")
         self.root.geometry("600x400")
         self.root.resizable(0, 0)
 
-        self.title_label = Label(root,text="输入原始内容：",anchor="center", font=("Microsoft YaHei", 11))
+        self.title_label = Label(__root, text="输入原始内容：", anchor="center", font=("Microsoft YaHei", 11))
         self.title_label.place(x=5, y=5, width=110, height=30)
 
-        self.original_text = Text(root)
+        self.original_text = Text(__root)
         self.original_text.place(x=5, y=40, width=573, height=140)
-        self.original_scroll = Scrollbar(root)
+        self.original_scroll = Scrollbar(__root)
         self.original_scroll.place(x=580, y=40, width=17, height=140)
-
-        # 两个控件关联
         self.original_scroll.config(command=self.original_text.yview)
         self.original_text.config(yscrollcommand=self.original_scroll.set)
-        # self.button_sestting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.OpenSetting)
-        # self.button_sestting.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
+
+        self.button_setting = Button(__root, text="打开设置", font=("Microsoft YaHei", 11), command=self.translate)
+        self.button_setting.place(x=5, y=190, width=50, height=50)
         #
         # self.button_setting = Button(root, text="打开设置", font=("Microsoft YaHei", 11), command=self.OpenSetting)
         # self.button_setting.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
@@ -29,6 +30,10 @@ class MainWindow:
     @staticmethod
     def OpenSetting():
         _ = SettingWindow(root)
+
+    def translate(self):
+        __original = self.original_text.get("1.0", "end-1c")
+        translates.fanyi(__original)
 
 
 class SettingWindow(Toplevel):
@@ -49,6 +54,7 @@ class SettingWindow(Toplevel):
 
 if __name__ == '__main__':
     util.file.init('./setting.json')
+    print(translates.languages)
     root = Tk()
     MainWindow(root)
     root.mainloop()
