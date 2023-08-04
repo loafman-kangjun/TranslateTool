c60de6995a3685e6d9eef7006e19f2a2f62762b7
LoafMan <kangjun1120@hotmail.com>
2023-08-04 15:42:12 +0800

    init

--- translates.py
+++ translates.py
@@ -1,6 +1,6 @@
 from translate import Translator
 
 if __name__ == '__main__':
-    translator = Translator(from_lang="zh-CN", to_lang="cu", email="kangjun1120@126.com", provider="mymemory")
+    translator = Translator(from_lang="zh-CN", to_lang="tl-PH", email="kangjun1120@126.com", provider="mymemory")
     translation = translator.translate("床前明月光，疑是地上霜;举头望明月,低头思故乡")
     print(translation)
